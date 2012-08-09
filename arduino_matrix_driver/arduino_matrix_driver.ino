16f5a4a71d18d0e13ba3f35cc9b5d9a80f8a786b
Phillip Pearson <pp@myelin.co.nz>
2012-08-09 01:37:53 -0700

    Tidied comments

--- arduino_matrix_driver/arduino_matrix_driver.ino
+++ arduino_matrix_driver/arduino_matrix_driver.ino
@@ -37,11 +37,9 @@ void fast_show() {
 */
 #define clkpinmask 0x10
 #define datapinmask 0x04
-//#define resetmask 0xeb
 
   for (uint8_t *stop_ptr = strip.pixels + strip.numPixels() * 3,  *pixel = strip.pixels; pixel != stop_ptr; ++pixel) {
     uint8_t pixel_value = *pixel;
-//#define BANG_WS2801_BIT(bit) PORTD = (PORTD & resetmask) | (pixel_value & bit ? datapinmask : 0); PORTD |= clkpinmask // this results in some odd flickering - too fast?
 #define BANG_WS2801_BIT(bit) if (pixel_value & bit) PORTD |= datapinmask; else PORTD &= ~datapinmask; PORTD |= clkpinmask; PORTD &= ~clkpinmask
     BANG_WS2801_BIT(0x80);
     BANG_WS2801_BIT(0x40);
@@ -56,29 +54,30 @@ void fast_show() {
   delay(1);
 }
 
+// byte available to read from the UART?
 #define serial_available() (UCSR0A & (1<<RXC0))
-#define serial_read() (UDR0)
-#define serial_can_write() (
 
+// read a byte from the UART
+#define serial_read() (UDR0)
+
+// is the UART ready for us to write a byte to it?
+#define serial_can_write() (UCSR0A & (1 << UDRE0))
+
+// write a byte to the UART
 void serial_write(uint8_t c) {
-  while (!(UCSR0A & (1 << UDRE0)));
+  while (!serial_can_write());
   UDR0 = c;
 }
 
+// write a string to the UART
 void serial_print(const char* s) {
   for (; *s; s++) {
-    while (!(UCSR0A & (1 << UDRE0)));
+    while (!serial_can_write());
     UDR0 = *s;
   }
 }
 
 void setup() {
-  // serial comms with host laptop
-  //Serial.begin(BAUD);
-  // disable serial rx interrupt - we'll deal with that ourselves
-  //unsigned char ucsr0b_udrie0 = UCSR0B & (1 << UDRIE0); // stash current state
-  //UCSR0B &= ~(1 << UDRIE0);
-
   // set LEDs weakly on in R/G/B pattern.
   strip.begin();
   uint8_t c = random(0, 255);
@@ -89,6 +88,7 @@ void setup() {
   }
   fast_show();
 
+  // set up UART
   UCSR0A |= 1<<U2X0;
   UCSR0B |= (1<<RXEN0 | 1<<TXEN0);
   uint16_t divider = (F_CPU / 4 / BAUD - 1) / 2;
@@ -99,10 +99,11 @@ void setup() {
   UDR0 = '*';
 
   // ready!
-  serial_print("_OK.\n");
+  serial_print("OK.\n");
 }
 
 void loop() {
+  //TODO: integrate patterns from test code and clean this up
   static uint8_t header_pos = 0;
   while (header_pos < 4) {
     while (!serial_available());
@@ -115,12 +116,15 @@ void loop() {
   while (current_pixel < BUFFER_SIZE) {
     while (!serial_available());
     strip.pixels[current_pixel++] = (uint8_t)serial_read();
-    //if (!(current_pixel % 100)) { Serial.println(current_pixel, DEC); }
   }
 
-  //strip.show();
+  // push current pattern to the LEDs
   fast_show();
+
+  // toggle the LED (debugging)
   digitalWrite(LED, !digitalRead(LED));
+
+  // prepare for next round
   header_pos = 0;
   current_pixel = 0;
   serial_print("OK.\n");
