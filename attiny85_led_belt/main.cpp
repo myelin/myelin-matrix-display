b3d7c8fcf6a16e992cb56162e5e28b971ca7f6e7
Phillip Pearson <pp@myelin.co.nz>
2012-08-25 22:40:57 -0700

    copied attiny85_driver project, changed file extensions, ws2801 config, and target

---
+++ attiny85_led_belt/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * ATTINY85 code to play random effects - for LED testing.
+ * Copyright (C) 2012 Phillip Pearson
+ * You may use this under a CC Attribution license.
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+#include "ws2801.h"
+
+uint8_t pixel_data[WS2801_BUFFER_LEN];
+
+void setup() {
+  ws2801_setup();
+  memset(pixel_data, 128, WS2801_BUFFER_LEN);
+  ws2801_show(pixel_data);
+}
+
+extern void run_effects();
+
+void loop() {
+  run_effects();
+}
+
+int main() {
+  setup();
+  while (1) loop();
+}
