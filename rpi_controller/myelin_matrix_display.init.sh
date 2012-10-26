31d7752d26b80131b8675d404f7dce1ca23d4de2
Phillip Pearson <pp@myelin.co.nz>
2012-10-26 00:33:13 -0700

    startup scripts

--- rpi_controller/myelin_matrix_display.init.sh
+++ rpi_controller/myelin_matrix_display.init.sh
@@ -2,10 +2,11 @@
 
 case $1 in
     start)
-	#/home/pi/rpi_controller/stop_daughterboard
+	/usr/sbin/matrix_proxy </dev/null 2>/dev/null >/dev/null &
 	;;
 
     stop)
+	killall matrix_proxy
 	/home/pi/rpi_controller/restart_daughterboard
 	;;
 esac
