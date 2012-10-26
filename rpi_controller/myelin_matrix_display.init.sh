957a9ef3d417c436aba7a12e671154eb00531e5e
Phillip Pearson <pp@myelin.co.nz>
2012-10-26 02:31:25 -0700

    more forceful restart

--- rpi_controller/myelin_matrix_display.init.sh
+++ rpi_controller/myelin_matrix_display.init.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-case $1 in
-    start)
-	rm /var/run/matrix_proxy/cmd
-	/usr/sbin/matrix_proxy </dev/null 2>/dev/null >/dev/null &
-	su - pi -c 'python rpi_controller/supervisor.py' </dev/null 2>/dev/null >/dev/null &
-	;;
-
-    stop)
+stop_matrix() {
 	killall matrix_proxy
 	killall ripples
 	killall bounce
@@ -15,7 +8,23 @@ case $1 in
 	killall epilepsy
 	killall strobe
 	killall rainbow
-	killall supervisor.py
+	kill `ps auxf | grep 'python rpi_controller/supervisor' | grep -v grep | awk '{print $2}'`
 	/home/pi/rpi_controller/restart_daughterboard
+}
+
+start_matrix() {
+	rm /var/run/matrix_proxy/cmd
+	/usr/sbin/matrix_proxy </dev/null 2>/dev/null >/dev/null &
+	su - pi -c 'python rpi_controller/supervisor.py' </dev/null 2>/dev/null >/dev/null &
+}
+
+case $1 in
+    start)
+	stop_matrix
+	start_matrix
+	;;
+
+    stop)
+	stop_matrix
 	;;
 esac
