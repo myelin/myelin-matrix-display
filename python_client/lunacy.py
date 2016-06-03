4df462b27a402e0064b986f7b86a5a1d0ca1a592
Phillip Pearson <pp@myelin.co.nz>
2016-06-02 18:07:34 -0700

    lunacy.py now works with new matrix.py

--- python_client/lunacy.py
+++ python_client/lunacy.py
@@ -7,10 +7,10 @@ def main():
     print pic
     print pic.size
 
-    flipper = matrix.Flipper(25)
-    f = matrix.Frame()
+    mx = matrix.Matrix('127.0.0.1', 25, 12, 50)
+    f = mx.frame()
     def flip():
-        flipper.flip(f)
+        mx.show(f)
 
     def draw_offset_mult(offset, blend, mult, mask):
         for y in range(min(f.height, pic.size[1])):
@@ -19,7 +19,7 @@ def main():
                     matrix.cmultiply(matrix.multiply(pic.getpixel((x + offset, y))[:3], mult * (1-blend)), mask),
                     matrix.cmultiply(matrix.multiply(pic.getpixel((x + offset + 1, y))[:3], mult * blend), mask)
                     )
-                f.point(x, y, pix)
+                f.point(x, y, [int(x) for x in pix])
 
     def fade(offset, blend, start, end, period):
         mask = matrix.random_mask()
@@ -32,15 +32,15 @@ def main():
     max_offset = pic.size[0] - f.width - 1
 
     def random_fade(frames):
-        start = flipper.frame
-        while (flipper.frame - start) < frames:
+        start = mx.frame_no
+        while (mx.frame_no - start) < frames:
             offset = random.randint(0, max_offset)
-    #        fade(offset, 0, 0.0, 1.0, flipper.frame_rate)
-            fade(offset, 0, 1.0, 0.0, flipper.frame_rate / 2)
+    #        fade(offset, 0, 0.0, 1.0, mx.frame_rate)
+            fade(offset, 0, 1.0, 0.0, mx.frame_rate / 2)
 
     def random_jitter(frames):
-        start = flipper.frame
-        while (flipper.frame - start) < frames:
+        start = mx.frame_no
+        while (mx.frame_no - start) < frames:
             offset = random.randint(0, max_offset)
             draw_offset_mult(offset, 0, 1.0, matrix.random_mask())
             flip()
@@ -59,19 +59,19 @@ def main():
             #for offset in range(max_offset):
             #for _blend in range(10):
             #blend = float(_blend) / 10
-            draw_offset_mult(offset, blend, 1.0, matrix.wheel_mask(flipper.frame % matrix.WHEEL_MAX))
+            draw_offset_mult(offset, blend, 1.0, matrix.wheel_mask(mx.frame_no % matrix.WHEEL_MAX))
             flip()
 
     def scroll_fade():
         for offset in range(0, max_offset, 5):
-            fade(offset, 0, 1.0, 0.0, flipper.frame_rate / 2)
+            fade(offset, 0, 1.0, 0.0, mx.frame_rate / 2)
 
     while 1:
         scroll_fade()
-        random_jitter(flipper.frame_rate * 2)
+        random_jitter(mx.frame_rate * 2)
         scroll_logo(max_offset / 2)
-        random_fade(flipper.frame_rate * 4)
-        random_jitter(flipper.frame_rate * 2)
+        random_fade(mx.frame_rate * 4)
+        random_jitter(mx.frame_rate * 2)
 
 
 if __name__ == '__main__':
