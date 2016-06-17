5dac83cea9db2c1fdd5c2c538ca3c780cb062d76
Matt Stephenson <mattstep@mattstep.net>
2016-06-17 16:24:51 -0700

    now with more pulsy hexagony thing

--- python_client/hexagons_sweepy_pulsy_thing.py
+++ python_client/hexagons_sweepy_pulsy_thing.py
@@ -14,11 +14,12 @@ matrix.sock.setblocking(0)
 
 frame_rate = 20
 brightness = 20
-tracelength = 96.0
+tracelength = 3*37.0
 mx = matrix.RawMatrix(['127.0.0.1', '192.168.0.81:6454'], 8, 8, frame_rate)
 sq = mx.frame() # 8x8 square at the start of the chain
 hexs = [mx.add_frame(matrix.HexagonFrame()) for i in range(7)]
-hex_rgb_constants = [[0.2,0.9,0.9],[0.8,0.8,0.3],[0.3,0.7,0.0],[1,0.3,0.3],[0,0,1],[1,1,1],[1,0,1]]
+hex_rgb_constants = [[0.2,0.9,0.9],[0.8,0.8,0.3],[0.3,0.7,0.0],[1,0.3,0.3],[0,0,1],[0.8,0.8,0.9],[0.9,0,1]]
+hex_gradient = [0.3,0.6,1.0,1.0,0.6,0.3,0.6]
 
 sq.dim(0)
 
@@ -27,10 +28,13 @@ while 1:
     n += 1
     print>>LOG, "OUT %s sent frame %d" % (time.time(), n)
 
+    multiplier = ((n / 20) % 2) * 0.07 + 0.99
+    
     #sq.point(randint(0, sq.width-1), randint(0, sq.height-1), random_color(brightness))
     for i,h in enumerate(hexs):
-        hex_brightness = brightness/2 + (i/6.0 + (n % tracelength)/tracelength)*(brightness/2)
+        hex_brightness = brightness/2 + (hex_gradient[i] + (n % tracelength)/tracelength)*(brightness/2)
         h.spiral_point(n % 37, [int(x * hex_brightness) for x in hex_rgb_constants[int((i + n/tracelength) % 7)]])
+        h.dim(multiplier)
 
     mx.show()
     try:
